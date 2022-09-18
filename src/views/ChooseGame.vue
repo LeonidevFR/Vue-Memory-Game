43a6c215d340e845b9c433f5bb0fffdf5372a7e3
LeonidevFR <leonardploton.dev@gmail.com>
2022-09-18 12:03:44 +0200

    Create ChooseGame component & create CSS class

---
+++ src/views/ChooseGame.vue
@@ -0,0 +1,19 @@
+<template>
+  <TheHeader></TheHeader>
+  <div class="flex justify-center">
+    <h1>What game do you want to play ?</h1>
+  </div>
+  <div class="flex justify-center">
+    <span
+      class="btn-secondary"
+      @click="$router.push({ name: 'memory-simon-says' })"
+    >
+      Simon Says
+    </span>
+    <span class="btn-secondary"> Number Memory </span>
+  </div>
+</template>
+<script setup>
+import TheHeader from "../components/TheHeader.vue";
+</script>
+<style lang=""></style>
