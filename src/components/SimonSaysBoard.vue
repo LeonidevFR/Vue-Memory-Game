4c7365652cc8ea809e5b0d64a2e1cd768a36b34d
LeonidevFR <leonardploton.dev@gmail.com>
2022-09-18 13:59:56 +0200

    Move Simon Says into its own component

---
+++ src/components/SimonSaysBoard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-center">
+    <div
+      v-for="(button, i) in buttons"
+      @click="$emit('onButtonClick', i)"
+      :class="`${button.color} ${button.class} ${
+        isButtonClickable ? 'cursor-pointer' : ''
+      }`"
+      class="mx-4 rounded-[50px] w-[50px] h-[50px] opacity-40 hover:opacity-80 active:opacity-100"
+    ></div>
+  </div>
+</template>
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  buttons: Array,
+  isButtonClickable: Boolean,
+});
+</script>
+<style lang=""></style>
